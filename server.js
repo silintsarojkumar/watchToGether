6890acb28af5da78a6055df4a188b35f9ff954b8
saroj <ersarojiitm@gmail.com>
2025-07-22 23:58:12 +0530

    port fixed

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
